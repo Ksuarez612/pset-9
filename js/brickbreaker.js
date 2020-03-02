e62d9d38a0676e084a71e098944092c79431053b
Kevin Suarez <kdock994@gmail.com>
2020-03-01 20:19:14 -0500

    bouncer

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -3,14 +3,22 @@ const ctx = canvas.getContext("2d");
 let counter = document.getElementById("wins")
 let speed;
 ctx.strokeStyle = "blue";
-let speed_change;
+let sc;
 let start = false;
 let blocks = [];
-let wins = 0;
+let nw = 0;
 let user = {
     x: (canvas.width / 2) - 40,
     y: canvas.height - 10,
     width: 80,
     height: 5,
     movement: 20
+};
+
+let bouncer = {
+    x: undefined,
+    y: undefined,
+    radius: 10,
+    right: true,
+    up: true
 };
