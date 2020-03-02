0e7ce83abb24e8be8aa66facaaf3f99a1c531e5e
Kevin Suarez <kdock994@gmail.com>
2020-03-02 17:21:13 -0500

    bb done

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,12 +1,12 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 let counter = document.getElementById("wins")
-let s;
-ctx.strokeStyle = "blue";
-let sc;
+let speed;
+ctx.strokeStyle = "black";
+let speed_change;
 let start = false;
 let blocks = [];
-let nw = 0;
+let wins = 0;
 let user = {
     x: (canvas.width / 2) - 40,
     y: canvas.height - 10,
@@ -14,8 +14,7 @@ let user = {
     height: 5,
     movement: 20
 };
-
-let bouncer = {
+let boinker = {
     x: undefined,
     y: undefined,
     radius: 10,
@@ -28,6 +27,7 @@ window.onload = function() {
     document.getElementById("game").onclick = init;
     game();
 }
+
 document.addEventListener("keydown", getArrowKeys);
 
 function init() {
@@ -37,8 +37,8 @@ function init() {
     boinker.up = true;
     user.x = (canvas.width / 2) - 40;
     user.y = canvas.height - 10;
-    sc = 1;
-    s = 0;
+    speed_change = 1;
+    speed = 0;
     blocks = [];
     createblocks();
     start = true;
@@ -58,51 +58,56 @@ function game() {
 }
 
 function changeDirection() {
-    if (bouncer.right) {
-        s = 3 * sc;
-    } else {
-        s = -3 * sc;
+    if (boinker.right) {
+        speed = 3 * speed_change;
     }
-    if (bouncer.up) {
+    else {
+        speed = -3 * speed_change;
+    }
+    if (boinker.up) {
         dy = -3;
-    } else {
+    }
+    else {
         dy = 3;
     }
-    bouncer.x += s;
-    bouncer.y += dy;
+    boinker.x += speed;
+    boinker.y += dy;
 }
 
 function checkCollision() {
-    if (bouncer.x - bouncer.radius <= 0) {
-        bouncer.right = true;
+    if (boinker.x - boinker.radius <= 0) {
+        boinker.right = true;
     }
-    if (bouncer.x + bouncer.radius >= canvas.width) {
+    if (boinker.x + boinker.radius >= canvas.width) {
         boinker.right = false;
     }
-    if (bouncer.y - bouncer.radius <= 0) {
+    if (boinker.y - boinker.radius <= 0) {
         boinker.up = false;
     }
-    if (bouncer.y - bouncer.radius >= canvas.height) {
+    if (boinker.y - boinker.radius >= canvas.height) {
         lose();
     }
 
     for (let j = 0; j < blocks.length; j++) {
-        if (bouncer.y - bouncer.radius <= blocks[j].y + blocks[j].height && bouncer.y - bouncer.radius > blocks[j].y + blocks[j].height - 5 && bouncer.x >= blocks[j].x - bouncer.radius && bouncer.x < blocks[j].x + blocks[j].width + bouncer.radius) {
+        if (boinker.y - boinker.radius <= blocks[j].y + blocks[j].height && boinker.y - boinker.radius > blocks[j].y + blocks[j].height - 5 && boinker.x >= blocks[j].x - boinker.radius && boinker.x < blocks[j].x + blocks[j].width + boinker.radius) {
             boinker.up = false;
             ctx.clearRect(blocks[j].x, blocks[j].y, blocks[j].width, blocks[j].height);
             blocks.splice(j, 1);
             break;
-        } else if (!bouncer.up && bouncer.y + bouncer.radius >= blocks[j].y && bouncer.y + bouncer.radius < blocks[j].y + 12 && bouncer.x >= blocks[j].x - bouncer.radius && bouncer.x < blocks[j].x + blocks[j].width + bouncer.radius) {
-            boinker.up = true;
-            ctx.clearRect(blocks[j].x, blocks[j].y, blocks[j].width, blocks[j].height);
-            blocks.splice(j, 1);
-            break;
-        } else if (bouncer.x + bouncer.radius >= blocks[j].x && bouncer.x + bouncer.radius < blocks[j].x + 10 && bouncer.y >= blocks[j].y - bouncer.radius && bouncer.y < blocks[j].y + blocks[j].height + bouncer.radius) {
+        }
+        else if (!boinker.up && boinker.y + boinker.radius >= blocks[j].y && boinker.y + boinker.radius < blocks[j].y + 12 && boinker.x >= blocks[j].x - boinker.radius && boinker.x < blocks[j].x + blocks[j].width + boinker.radius) {
+          boinker.up = true;
+          ctx.clearRect(blocks[j].x, blocks[j].y, blocks[j].width, blocks[j].height);
+          blocks.splice(j, 1);
+          break;
+        }
+        else if (boinker.x + boinker.radius >= blocks[j].x && boinker.x + boinker.radius < blocks[j].x + 10 && boinker.y >= blocks[j].y - boinker.radius && boinker.y < blocks[j].y + blocks[j].height + boinker.radius) {
             boinker.right = false;
             ctx.clearRect(blocks[j].x, blocks[j].y, blocks[j].width, blocks[j].height);
             blocks.splice(j, 1);
             break;
-        } else if (bouncer.x - bouncer.radius <= blocks[j].x + blocks[j].width && bouncer.x - bouncer.radius > blocks[j].x + blocks[j].width - 10 && bouncer.y >= blocks[j].y - bouncer.radius && bouncer.y < blocks[j].y + blocks[j].height + bouncer.radius) {
+        }
+        else if (boinker.x - boinker.radius <= blocks[j].x + blocks[j].width && boinker.x - boinker.radius > blocks[j].x + blocks[j].width - 10 && boinker.y >= blocks[j].y - boinker.radius && boinker.y < blocks[j].y + blocks[j].height + boinker.radius) {
             boinker.right = true;
             ctx.clearRect(blocks[j].x, blocks[j].y, blocks[j].width, blocks[j].height);
             blocks.splice(j, 1);
@@ -110,25 +115,27 @@ function checkCollision() {
         }
     }
 
-    if (bouncer.y + bouncer.radius == user.y) {
+    if (boinker.y + boinker.radius == user.y) {
         let a = 3;
-        const sc_change = a / 25;
+        const speed_change_change = a / 25;
         for (let i = 2; i <= 100; i += 2) {
-            if (bouncer.x >= user.x - bouncer.radius + i - 2 && bouncer.x < user.x - bouncer.radius + i) {
+            if (boinker.x >= user.x - boinker.radius + i - 2 && boinker.x < user.x - boinker.radius + i) {
                 if (i < 50) {
-                    bouncer.up = true;
-                    bouncer.right = false;
-                    s = (s >= 11) ? s = 11 : s + 0.5;
-                    sc = Math.abs(a);
-                } else if (i >= 50) {
-                    bouncer.up = true;
-                    bouncer.right = true;
-                    s = (s >= 11) ? s = 11 : s + 0.5;
-                    sc_change = Math.abs(a);
+                    boinker.up = true;
+                    boinker.right = false;
+                    speed = (speed >= 11) ? speed = 11 : speed + 0.5;
+                    speed_change = Math.abs(a);
+                }
+                else if (i >= 50) {
+                    boinker.up = true;
+                    boinker.right = true;
+                    speed = (speed >= 11) ? speed = 11 : speed + 0.5;
+                    speed_change = Math.abs(a);
                 }
                 break;
-            } else {
-                a -= sc_change;
+            }
+            else {
+                a -= speed_change_change;
             }
         }
     }
@@ -136,10 +143,12 @@ function checkCollision() {
     if (user.x + user.width > canvas.width) {
         user.movement = 0;
         user.x = canvas.width - user.width;
-    } else if (user.x < 0) {
+    }
+    else if (user.x < 0) {
         user.movement = 0;
         user.x = 0;
-    } else {
+    }
+    else {
         user.movement = 20;
     }
 }
@@ -147,7 +156,7 @@ function checkCollision() {
 function draw() {
     ctx.strokeRect(user.x, user.y, user.width, user.height);
     ctx.beginPath();
-    ctx.arc(bouncer.x, bouncer.y, bouncer.radius, 0, Math.PI * 2);
+    ctx.arc(boinker.x, boinker.y, boinker.radius, 0, Math.PI * 2);
     ctx.stroke();
     for (let i = 0; i < blocks.length; i++) {
         ctx.strokeRect(blocks[i].x, blocks[i].y, blocks[i].width, blocks[i].height);
@@ -158,7 +167,8 @@ function getArrowKeys(event) {
     if (start) {
         if (event.keyCode == 37) {
             moveuser(-1 * user.movement);
-        } else if (event.keyCode == 39) {
+        }
+        else if (event.keyCode == 39) {
             moveuser(user.movement);
         }
     }
@@ -171,13 +181,13 @@ function moveuser(pixels) {
 function createblocks() {
     for (let y = 0; y <= 80; y += 40) {
         for (let x = 0; x < canvas.width; x += canvas.width / 10) {
-            let bTemplate = {
+            let bottleTemplate = {
                 x: x,
                 y: y,
                 width: canvas.width / 10,
                 height: 40
             };
-            blocks.push(bTemplate);
+            blocks.push(bottleTemplate);
         }
     }
 }
@@ -187,14 +197,14 @@ function lose() {
 }
 
 function win() {
-    start = false;
-    wins += 1
-    counter.innerHTML = wins
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "blue";
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2.5;
-    ctx.textAlign = "center";
-    ctx.font = "60px Arial";
-    ctx.fillText("You Win!", canvas.width / 2, (canvas.height / 2) - 40);
+  start = false;
+  wins += 1
+  counter.innerHTML = wins
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "gray";
+  ctx.lineWidth = 2.5;
+  ctx.textAlign = "center";
+  ctx.font = "60px Arial";
+  ctx.fillText("You Win!", canvas.width/2, (canvas.height/2) - 40);
 }
